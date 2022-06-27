72c64e220ed68bbfb603c6272638202e6cdff81c
matiasmilito <matiasmilito99@gmail.com>
2022-06-27 18:07:51 -0300

    modificaciones navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -84,7 +84,7 @@ const Navbar = () => {
                       </a>
                   </li>
                   <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]">
-                      <a className="flex justify-between items-center w-full text-gray-300" href="/">
+                      <a className="flex justify-between items-center w-full text-gray-300" href="https://drive.google.com/file/d/1WRK19h8IjlKG28CVjT0rKixYh3eBAk9u/view?usp=sharing">
                           Curriculum <BsFillPersonLinesFill size={30}/>
                       </a>
                   </li>
